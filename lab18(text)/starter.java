5e75008933da002cce6fbcbf7fc8f238b417e2dd
polyqubit <polyumbrial@gmail.com>
2021-09-02 16:11:21 -0700

    lab18

---
+++ lab18(text)/starter.java
@@ -0,0 +1,27 @@
+import pkg.*;
+import java.util.ArrayList;
+
+public class starter implements InputControl, InputKeyControl {
+
+	public static void main(String args[]) {
+		// please leave following line alone, necessary for keyboard/mouse input
+		KeyController kC = new KeyController(Canvas.getInstance(),new starter());
+		MouseController mC = new MouseController(Canvas.getInstance(),new starter());
+		
+		Text written = new Text(100.0, 100.0, "hi");
+		Color redText = Color.RED;
+		written.setColor(redText);
+		written.grow(50.0,20.0);
+		written.draw();
+	}
+
+	public void onMouseClick(double x, double y) {
+		// enter code here
+
+	}
+
+	public void keyPress(String s) {
+		// enter code here
+
+	}
+}
