5455a5ca7293bde3454bd2435bf6da0f55ff2232
polyqubit <polyumbrial@gmail.com>
2021-09-02 15:03:05 -0700

    lab14

---
+++ lab14/starter.java
@@ -0,0 +1,41 @@
+import pkg.*;
+import java.util.ArrayList;
+
+public class starter implements InputControl, InputKeyControl {
+
+	public static void main(String args[]) {
+		// please leave following line alone, necessary for keyboard/mouse input
+		
+		// lol no
+		
+		
+		int guessnumber = Canvas.rand(1001);
+		boolean guessedBool = false;
+		while(guessedBool == false) {
+			System.out.print("Number guessing game! Guess the number between 1 and 1000!\n");
+			EasyReader guessIn = new EasyReader();
+			String guessed = guessIn.readWord();
+			int guessint = Integer.parseInt(guessed);
+			if(guessint == guessnumber){
+				System.out.print("Yes YEs1!");
+				break;
+			}
+			else if(guessint < guessnumber){
+				System.out.print("Too low! Try !\n\n");				
+			}
+			else if(guessint > guessnumber){
+				System.out.print("Too high! Try !\n\n");
+			}
+		}
+	}
+
+	public void onMouseClick(double x, double y) {
+		// enter code here
+
+	}
+
+	public void keyPress(String s) {
+		// enter code here
+
+	}
+}
